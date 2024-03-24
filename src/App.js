69735447f9e9d7e963aec95fcf6c1add4cf9213d
sai8151 <saikiranreddy8151@gmail.com>
2024-03-24 22:30:32 +0530

    #e

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const [userGuess, setuserguess] = useState(0);
+  const [userCount, setUserCount] = useState(0);
+  const [GuessedValues, setGuessedValues] = useState([]);
+  const [number, setnumber] = useState(0);
+  const [numberValues, setnumberValues] = useState([]);
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [GuessedValues]);
+
+  const HandelValueChange = (e) => {
+    setuserguess(e.target.value);
+  }
+  const randomNumberInRange = (min, max) => {
+    return Math.floor(Math.random()
+      * (max - min + 1)) + min;
+  };
+  const SubmitHandler = () => {
+    // alert(userGuess)
+    setUserCount(userCount + 1)
+    setGuessedValues([...GuessedValues, userGuess])
+    setnumberValues([...numberValues, number])
+    setnumber(randomNumberInRange(1, 20))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>Guess the number (1 to 20):</h1>
+        <input
+          type="range"
+          value={userGuess}
+          onChange={HandelValueChange}
+          min='1'
+          max='20'
+          className="slider"
+          id="range"
+        />
+        <p>Value: {userGuess}</p>
+
+
+        <button onClick={SubmitHandler}>Submit</button>
+        <p>Number of guesses : {userCount}</p>
+        <p>userGuess :
+          <span id="guesses" ref={scrollRef} className="scroll-container">
+            {GuessedValues?.map((item, index) => {
+              if (item == numberValues[index]) {
+                return (
+                  <h6 id="correct" key={index}>Congratulations! You guessed {item} correctly!</h6>
+                );
+              } else {
+                return (
+                  <h6 id="incorrect" key={index}>You guessed {item}, but the number is {numberValues[index]}</h6>
+                );
+              }
+            })}
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
